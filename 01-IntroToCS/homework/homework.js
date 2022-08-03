64731735fb93d7fa25d7ea93702171e9f313d627
Jorge Duran <andresmau300@gmail.com>
2022-08-02 21:59:03 -0500

    Finished 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,25 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  num = String(num)
+  binarioR = num.split('').reverse().join('');
+  let Decimal = 0;
+  for (let i = 0; i < num.length ; i++){
+      Decimal = Decimal + binarioR.charAt(i) * Math.pow(2 , i);
+  }
+  return Decimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let Binario = []
+  while (num!==0){
+      let residuo = parseInt(num%2)
+      num = Math.floor(num/2);
+      Binario.push(residuo);
+  }
+  Binario = Binario.reverse();
+  return Binario.join('');
 }
 
 
